d03f232da1b38ec56fd401441e3a826108e7cf49
Darko <Darko@Darko-PC>
2014-09-22 20:55:40 +0200

    detalji kupca preko id-a

--- search.php
+++ search.php
@@ -4,13 +4,14 @@ include 'baza/konekcija.php';
 if (isset($_POST['value'])){  #isset miče sve warninge, prvo provjerava dali je postavljena vrijednost pa ide dalje sa kodom
 $value = $_POST['value'];
 echo '<ul>';
-$query = mysql_query("SELECT ime, prezime, tvrtka FROM fisk_kupac WHERE ime LIKE '$value%' OR prezime LIKE '$value%'");
+$query = mysql_query("SELECT id, ime, prezime, tvrtka FROM fisk_kupac WHERE ime LIKE '$value%' OR prezime LIKE '$value%'");
 $result = $query or die(mysql_error()); 
 while ($run = mysql_fetch_array($result)){
+    $id = $run['id'];
     $ime = $run['ime'];
     $prezime = $run['prezime'];
     $tvrtka = $run['tvrtka'];
-    echo '<li><a class="a" href=#>'.$ime.' '.$prezime.', '.$tvrtka.'</a></li>';
+    echo '<li><a class="a" href="kupacDet.php?id=' .$id. '">'.$ime.' '.$prezime.', '.$tvrtka.'</a></li>';
 }
 echo '</ul>';
 }
