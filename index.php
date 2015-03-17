e8afc733f172a9683780ada8e5f0c0f5e5b85d27
ServisNjic <ServisNjic@ServisNjic-PC>
2015-03-17 14:32:33 +0100

    footer na dno

--- index.php
+++ index.php
@@ -57,9 +57,12 @@ include 'init.php';
                             </div>
                         </div>
                     </div>
-                     <?php include 'dijeloviHTML/sections.php'; ?>
+                     
                 </div>
+                <div>
+                <?php include 'dijeloviHTML/sections.php'; ?>
                 <?php include 'dijeloviHTML/footer.php';?>
+                </div>
             </div>
         </div>
 
