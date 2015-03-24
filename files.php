760609a83fece60b634891825e775350fceafd6e
Korisnik <Korisnik@Korisnik-PC>
2015-03-24 22:16:59 +0100

    upload-button

--- files.php
+++ files.php
@@ -14,7 +14,8 @@ include 'init.php';
         <meta name="keywords" content="Eurotrade, Servis, Eurotrade servis">
         <link href="css/style.css" rel="stylesheet" type="text/css">
         <link href="css/table.css" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     </head>
     <body>
 
@@ -40,29 +41,35 @@ include 'init.php';
                                 ?>
                                 <form method="POST" enctype="multipart/form-data">
                                     <input type="hidden" name="upload" value="1">
-                                    <input type="file" name="file">
+                                    <div class="input-group">
+                                        <span class="input-group-btn">
+                                            <span class="btn btn-primary btn-file">
+                                                Browse&hellip; <input type="file" multiple>
+                                            </span>
+                                        </span>
+                                        <input type="text" class="form-control" readonly>
+                                    </div>
                                     <input type="submit" value="Upload">
                                 </form>
 
                                 <h1 class="title">Upute za postavljenje<span> kase u rad:</span></h1>
-                               
+
                                 <?php
                                 if ($handle = opendir('pdf/')) {
                                     while (false !== ($file = readdir($handle))) {
-                                        
+
                                         if (($file != ".") && ($file != "..")) {
-                                            $kb=filesize("pdf/$file ") / 1024;
-                                            $velicina = round($kb,2);
+                                            $kb = filesize("pdf/$file ") / 1024;
+                                            $velicina = round($kb, 2);
                                             $thelist .= '<tr>'
                                                     . '<td>'
                                                     . '<a href="pdf/' . $file . '">' . $file . '</a>'
                                                     . '</td>'
-                                                    . '<td>'. pathinfo("pdf/$file ", PATHINFO_EXTENSION) .'</td>'
+                                                    . '<td>' . pathinfo("pdf/$file ", PATHINFO_EXTENSION) . '</td>'
                                                     . '<td>' . $velicina . '</td>'
-                                                    . '<td>' . date ("d. m. Y H:i:s.",filemtime("pdf/$file ")) . '</td>'
+                                                    . '<td>' . date("d. m. Y H:i:s.", filemtime("pdf/$file ")) . '</td>'
                                                     . '</tr>';
-                                            /*$thelist .= '<li><a href="pdf/' . $file . '">' . $file . '</a>';*/
-                                        
+                                            /* $thelist .= '<li><a href="pdf/' . $file . '">' . $file . '</a>'; */
                                         }
                                     }
 
@@ -74,13 +81,13 @@ include 'init.php';
                                     <th>Datoteka</th>
                                     <th>Tip</th>
                                     <th>Veličina (kb)</th>
-                                    <th>Datum dodavanja</th>
+                                    <th>Izmijenjeno</th>
                                     </thead>
                                     <?= $thelist ?>
                                 </table>
-                               <!-- <UL>
-                                    <P><?= $thelist ?></p>
-                                </UL> -->
+                                <!-- <UL>
+                                     <P><?= $thelist ?></p>
+                                 </UL> -->
                                 <p>&nbsp;</p>
                                 <p>&nbsp;</p>
                             </div>
@@ -128,8 +135,31 @@ include 'init.php';
                 </div>
             </div>
         </div>
-<?php include 'dijeloviHTML/footer.php'; ?>
+        <?php include 'dijeloviHTML/footer.php'; ?>
+        <script>
+            $(document).on('change', '.btn-file :file', function () {
+                var input = $(this),
+                        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+                        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+                input.trigger('fileselect', [numFiles, label]);
+            });
 
+            $(document).ready(function () {
+                $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+                    var input = $(this).parents('.input-group').find(':text'),
+                            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                    if (input.length) {
+                        input.val(log);
+                    } else {
+                        if (log)
+                            alert(log);
+                    }
+
+                });
+            });
+        </script>
 
     </body>
 </html>
